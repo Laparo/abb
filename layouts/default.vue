1cbc142a3df07d9e055e1f9d8662821955b2b941
Andreas Bernt-Baertl <abb@laparo.biz>
2025-09-26 20:23:57 +0200

    chore: remove stray layout to clean working tree before /clarify

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main tag="main" role="main">
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+// Standardlayout für Vuetify-Wrapper
+defineOptions({
+  name: 'DefaultLayout',
+})
+</script>
